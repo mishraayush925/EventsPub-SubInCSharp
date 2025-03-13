348776ec11476ee271a594a3111cdd510e5c7287
mishraayush925 <61752344+mishraayush925@users.noreply.github.com>
2025-03-13 21:58:51 +0530

    2nd commit for events

--- LearnEevntsInCSharpProj/VideoEncoder.cs
+++ LearnEevntsInCSharpProj/VideoEncoder.cs
@@ -9,13 +9,29 @@ namespace LearnEevntsInCSharpProj
 {
     public class VideoEncoder
     {
+        // 1 define  a delegate - may use built in delegates here 
+        // 2 define an event based on that delegate 
+        // 3 raise the event 
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+        public event VideoEncodedEventHandler VideoEncoded;
+
         public void EncodeVideo(Video video)
         {
             Console.WriteLine("Encoding {0} video",video.Title);
 
             Thread.Sleep(2000);
 
-            Console.WriteLine("Video {0} encoded ",video.Title);
+            OnVideoEncoded(video.Title);
+        }
+
+        protected virtual void OnVideoEncoded(string title)
+        {
+            if (VideoEncoded != null)
+            {
+                Console.WriteLine("video {0}encoded successfully",title);
+                VideoEncoded(this, EventArgs.Empty);
+            }
         }
     }
 }
