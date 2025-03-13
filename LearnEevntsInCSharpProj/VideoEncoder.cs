d27c723ac2dbf5852872eb1003622f2b950fc706
mishraayush925 <61752344+mishraayush925@users.noreply.github.com>
2025-03-14 00:01:38 +0530

    Update VideoEncoder.cs with comments

--- LearnEevntsInCSharpProj/VideoEncoder.cs
+++ LearnEevntsInCSharpProj/VideoEncoder.cs
@@ -1,45 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
+using System;  // Importing the System namespace for basic functionalities
+using System.Collections.Generic; // Importing List<T> for storing encoded video titles
+using System.Threading; // Importing Thread to simulate encoding delay
 
 namespace LearnEevntsInCSharpProj
 {
+    // Custom EventArgs class to hold encoded video details
     public class VideoEventArgs : EventArgs
     {
-        public List<string> EncodedVideosList = new List<string>() ;
-       public VideoEventArgs(string str1) { 
-        EncodedVideosList.AddRange(str1.Split(" ")) ;
+        // List to store the names of encoded videos
+        public List<string> EncodedVideosList = new List<string>();
+
+        // Constructor: Takes a string of video names separated by space and splits them into a list
+        public VideoEventArgs(string str1) 
+        { 
+            EncodedVideosList.AddRange(str1.Split(" ")); 
         }
     }
+
+    // VideoEncoder class responsible for video encoding and event handling
     public class VideoEncoder
     {
-        // 1 define  a delegate - may use built in delegates here 
-        // 2 define an event based on that delegate 
-        // 3 raise the event 
-
+        // Step 1: Define a delegate that specifies the signature of the event handler
         public delegate void VideoEncodedEventHandler(VideoEncoder source, VideoEventArgs args);
+
+        // Step 2: Define an event based on the delegate
         public event VideoEncodedEventHandler VideoEncoded;
 
-       
+        // Static property to store encoded video details
         public static string EncodedVideoDetails { get; set; }
+
+        // Method to encode a video (simulated with Thread.Sleep for delay)
         public void EncodeVideo(Video video)
         {
-            Console.WriteLine("Encoding {0} video",video.Title);
-            EncodedVideoDetails = video.Title;
+            Console.WriteLine("Encoding {0} video", video.Title);
+            EncodedVideoDetails = video.Title; // Store the encoded video title
 
-            Thread.Sleep(2000);
+            Thread.Sleep(2000); // Simulating video encoding time
 
+            // Step 3: Raise the event after encoding is complete
             AfterEncoding(video.Title);
         }
 
+        // Protected method to notify subscribers after encoding is completed
         protected virtual void AfterEncoding(string title)
         {
-            if (VideoEncoded != null)
+            if (VideoEncoded != null) // Check if there are any subscribers to the event
             {
-                Console.WriteLine(" {0} encoded successfully",title);
+                Console.WriteLine(" {0} encoded successfully", title);
+
+                // Raising the event and passing the encoded video title as an argument
                 VideoEncoded(this, new VideoEventArgs(title));
             }
         }
