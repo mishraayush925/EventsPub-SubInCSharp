cfb4d9e8319aff17c8dee459816fc8ecbf028b8d
mishraayush925 <61752344+mishraayush925@users.noreply.github.com>
2025-03-13 21:37:04 +0530

    Add project files.

---
+++ LearnEevntsInCSharpProj/Program.cs
@@ -0,0 +1,15 @@
+﻿using LearnEevntsInCSharpProj;
+using System;
+namespace LearnEventsInCSharpProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello from events");
+            VideoEncoder encoder = new VideoEncoder();
+            encoder.EncodeVideo(new Video() { Title = "first video" });
+
+        }
+    }
+}
---
+++ LearnEevntsInCSharpProj/VideoEncoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace LearnEevntsInCSharpProj
+{
+    public class VideoEncoder
+    {
+        public void EncodeVideo(Video video)
+        {
+            Console.WriteLine("Encoding {0} video",video.Title);
+
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Video {0} encoded ",video.Title);
+        }
+    }
+}
